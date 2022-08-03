17c089751d87d27de501408315d447eba8d93767
wxomi <elif.alyoruk@ozu.edu.tr>
2022-08-03 14:20:17 +0530

    optimized app.js

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
-let counter=0;
+let count =0;
 
-const decrease=document.querySelector(".decrease");
-const reset=document.querySelector(".reset");
-const increase=document.querySelector(".increase");
+const value = document.getElementById("span");
+const btns= document.querySelectorAll(".btn");
 
-
-const span=document.getElementById("span");
-
-decrease.addEventListener("click", function(){
-    counter--;
-    if(counter<0){
-        span.style.color="red";
-    }else if(counter==0){
-        span.style.color="black";
-    }
-    console.log(counter);
-    span.textContent=counter;
-});
-reset.addEventListener("click", function(){
-    counter=0;
-    span.style.color="black";
-    span.textContent=counter;
-});
-increase.addEventListener("click", function(){
-    counter++;
-    if(counter>0){
-        span.style.color="green";
-    }else if(counter==0){
-        span.style.color="black";
-    }
-    console.log(counter);
-    span.textContent=counter;
+btns.forEach(function (btn){
+    btn.addEventListener("click",function(e){
+        const styles=e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }else if(styles.contains("increase")){
+            count++;
+        }else{
+            count=0;
+        }
+        if(count>0){
+            value.style.color="green";
+        }
+        if(count<0){
+            value.style.color="red";
+        }
+        if(count==0){
+            value.style.color="black"
+        }
+        value.textContent=count;
+    });
 });
